dfe7a37792b0b7f71af950a38e5727d6e2d971fc
Preethaa Dutta <92708433+preethaadutta@users.noreply.github.com>
2024-06-22 09:10:44 +0530

    Add files via upload

---
+++ Reactjs_Tutorial_Codewithharry/Navbar/Reactjs-navbar/src/App.js
@@ -0,0 +1,43 @@
+//import logo from './logo.svg';
+import './App.css';
+import {Navbar,Nav,Container} from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    /*
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    */
+    <div className="App">
+      <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      </>
+    </div>
+  );
+}
+
+export default App;
